cf95a50da3feb9992ea2a90b0450feea8f6ae232
hammadahmed713 <hammad@cloudcoherence.io>
2025-08-13 15:07:34 +0500

    al-rais

---
+++ src/components/molecules/PartnersSection.tsx
@@ -0,0 +1,55 @@
+import React, { useRef, useState } from "react";
+
+import emiratesIcon from "../../assets/images/emirates.png";
+import qatarIcon from "../../assets/images/qatar.png";
+import airlineIcon3 from "../../assets/images/airline_icon_3.png";
+import flydubaiIcon from "../../assets/images/flydubai_icon.jpeg";
+import pkAirlineIcon from "../../assets/images/pakistan_international_airlines_icon.jpeg";
+import airlineIcon6 from "../../assets/images/airline_icon_6.jpeg";
+import lufthansa_cityline_icon from "../../assets/images/lufthansa_cityline_icon.jpeg";
+import virginAtlanticIcon from "../../assets/images/virgin_atlantic_icon.jpeg";
+import americanAirlineIcon from "../../assets/images/american_airlines_icon.jpeg";
+import swiss_international_airlines_icon from "../../assets/images/swiss_international_airlines_icon.jpeg";
+import sas_airlines_icon from "../../assets/images/sas_-_scandinavian_airlines_icon.jpeg";
+
+const PartnersSection: React.FC = () => {
+  return (
+    <section className="bg-[#FFFFFF]">
+      <div className="w-full max-w-[1040px] !mt-20 m-auto">
+        <h5 className="text-[26px] text-center text-[rgba(10, 12, 15, 1)] mt-20 mb-1">
+          We are partnered up with the best
+        </h5>
+
+        <div className="flex justify-center items-center mt-10 gap-x-4">
+          <img src={emiratesIcon} alt="" className="w-[62px] h-[62px]" />
+          <img src={qatarIcon} alt="" className="w-[62px] h-[62px]" />
+          <img src={airlineIcon3} alt="" className="w-[62px] h-[62px]" />
+          <img src={flydubaiIcon} alt="" className="w-[62px] h-[62px]" />
+          <img src={pkAirlineIcon} alt="" className="w-[62px] h-[62px]" />
+          <img src={airlineIcon6} alt="" className="w-[62px] h-[62px]" />
+          <img
+            src={lufthansa_cityline_icon}
+            alt=""
+            className="w-[62px] h-[62px]"
+          />
+          <img src={virginAtlanticIcon} alt="" className="w-[62px] h-[62px]" />
+          <img src={americanAirlineIcon} alt="" className="w-[62px] h-[62px]" />
+          <img
+            src={swiss_international_airlines_icon}
+            alt=""
+            className="w-[62px] h-[62px]"
+          />
+          <img src={sas_airlines_icon} alt="" className="w-[62px] h-[62px]" />
+        </div>
+
+        <div className="text-center mt-10">
+          <a href="#" className="text-[12px] text-[#3D495C] opacity-80">
+            and many more...
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default PartnersSection;
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,7 +1,12 @@
-import React, { useRef } from 'react';
-import { useOutletContext } from 'react-router-dom';
-import { MainLayout } from '../components';
-import HeroSection from '../components/molecules/HeroSection';
+import React, { useRef } from "react";
+import { useOutletContext } from "react-router-dom";
+import { MainLayout } from "../components";
+import HeroSection from "../components/molecules/HeroSection";
+import PopularDestination from "../components/molecules/PopularDestination";
+import BestDeals from "../components/molecules/BestDeals";
+import PartnersSection from "../components/molecules/PartnersSection";
+import WhyChooseUs from "../components/molecules/WhyChooseUsSection";
+import ReadyToFlySection from "../components/molecules/ReadyToFlySection";
 
 interface LandingPageContext {
   onLoginClick: () => void;
@@ -9,17 +14,23 @@ interface LandingPageContext {
 }
 
 const LandingPage: React.FC = () => {
-  const { onLoginClick, onSignupClick } = useOutletContext<LandingPageContext>();
-
+  const { onLoginClick, onSignupClick } =
+    useOutletContext<LandingPageContext>();
 
   return (
     <MainLayout onLoginClick={onLoginClick} onSignupClick={onSignupClick}>
       <div className="relative justify-center flex flex-col max-w-full">
         <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-semibold text-black leading-tight mt-10 text-center">
-          Experience the true <span className="text-[#EA0029]">richness</span> of travel.
+          Experience the true <span className="text-[#EA0029]">richness</span>{" "}
+          of travel.
         </h1>
 
         <HeroSection />
+        <PopularDestination />
+        <BestDeals />
+        <PartnersSection />
+        <WhyChooseUs />
+        <ReadyToFlySection />
       </div>
     </MainLayout>
   );
